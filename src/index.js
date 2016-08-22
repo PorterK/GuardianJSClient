45004caefb3482f82ebfe9164aa2da8ee307c316
PorterK <whyisumad@me.com>
2016-08-21 22:02:16 -0400

    Updated a few things, removed the need for .connect

--- src/index.js
+++ src/index.js
@@ -4,16 +4,4 @@ const Promise = require('bluebird');
 
 const connect = require('./Connect').Connect;
 
-let testConnection = function(key, secured){
-  
-  let http = secure ? 'https://' : 'http://';
-
-  let testPromise = Promise.promisify(request.get);
-
-  return testPromise(`${http}content.guardianapis.com/?api-key=${key}`);
-}
-
-module.exports = {
-  testConnection,
-  connect
-};
+module.exports = connect;
