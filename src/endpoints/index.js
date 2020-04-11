e3c0fad235e8a1a67eadca01ed3e45a4367c4a8c
Kalob Porter <kalob@kalob.net>
2020-04-11 19:08:55 -0400

    Remove code duplication

--- src/endpoints/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: require('./Content').content,
-  tags: require('./Tags').tags,
-  sections: require('./Sections').sections,
-  editions: require('./Editions').editions,
-  item: require('./Item').item,
-  custom: require('./Custom').custom
-}
